e2c67f79de856d546402c1a0ff90836f2565e0a4
elitequant <contact@elitequant.com>
2018-01-21 10:27:02 -0500

    add qt language
    gt language

--- source/eqtrader/build/ui_mainwindow.h
+++ source/eqtrader/build/ui_mainwindow.h
@@ -367,8 +367,8 @@ public:
         gridLayout->addWidget(label, 0, 0, 1, 1);
 
         tableWidget_2 = new QTableWidget(layoutWidget);
-        if (tableWidget_2->columnCount() < 5)
-            tableWidget_2->setColumnCount(5);
+        if (tableWidget_2->columnCount() < 7)
+            tableWidget_2->setColumnCount(7);
         QTableWidgetItem *__qtablewidgetitem15 = new QTableWidgetItem();
         tableWidget_2->setHorizontalHeaderItem(0, __qtablewidgetitem15);
         QTableWidgetItem *__qtablewidgetitem16 = new QTableWidgetItem();
@@ -379,6 +379,10 @@ public:
         tableWidget_2->setHorizontalHeaderItem(3, __qtablewidgetitem18);
         QTableWidgetItem *__qtablewidgetitem19 = new QTableWidgetItem();
         tableWidget_2->setHorizontalHeaderItem(4, __qtablewidgetitem19);
+        QTableWidgetItem *__qtablewidgetitem20 = new QTableWidgetItem();
+        tableWidget_2->setHorizontalHeaderItem(5, __qtablewidgetitem20);
+        QTableWidgetItem *__qtablewidgetitem21 = new QTableWidgetItem();
+        tableWidget_2->setHorizontalHeaderItem(6, __qtablewidgetitem21);
         tableWidget_2->setObjectName(QStringLiteral("tableWidget_2"));
 
         gridLayout->addWidget(tableWidget_2, 1, 0, 1, 4);
@@ -431,94 +435,98 @@ public:
 
     void retranslateUi(QMainWindow *MainWindow)
     {
-        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "\347\276\244\350\213\261\351\207\217\345\214\226C++\347\211\210", 0));
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "EliteQuant", 0));
         QTableWidgetItem *___qtablewidgetitem = tableWidget->horizontalHeaderItem(0);
-        ___qtablewidgetitem->setText(QApplication::translate("MainWindow", "\345\220\210\347\272\246\344\273\243\347\240\201", 0));
+        ___qtablewidgetitem->setText(QApplication::translate("MainWindow", "Symbol", 0));
         QTableWidgetItem *___qtablewidgetitem1 = tableWidget->horizontalHeaderItem(1);
-        ___qtablewidgetitem1->setText(QApplication::translate("MainWindow", "\345\220\210\347\272\246\345\220\215\347\247\260", 0));
+        ___qtablewidgetitem1->setText(QApplication::translate("MainWindow", "Name", 0));
         QTableWidgetItem *___qtablewidgetitem2 = tableWidget->horizontalHeaderItem(2);
-        ___qtablewidgetitem2->setText(QApplication::translate("MainWindow", "\346\234\200\346\226\260\344\273\267", 0));
+        ___qtablewidgetitem2->setText(QApplication::translate("MainWindow", "Last", 0));
         QTableWidgetItem *___qtablewidgetitem3 = tableWidget->horizontalHeaderItem(3);
-        ___qtablewidgetitem3->setText(QApplication::translate("MainWindow", "\346\210\220\344\272\244\351\207\217", 0));
+        ___qtablewidgetitem3->setText(QApplication::translate("MainWindow", "Volume", 0));
         QTableWidgetItem *___qtablewidgetitem4 = tableWidget->horizontalHeaderItem(4);
-        ___qtablewidgetitem4->setText(QApplication::translate("MainWindow", "\346\214\201\344\273\223\351\207\217", 0));
+        ___qtablewidgetitem4->setText(QApplication::translate("MainWindow", "Open Interest", 0));
         QTableWidgetItem *___qtablewidgetitem5 = tableWidget->horizontalHeaderItem(5);
-        ___qtablewidgetitem5->setText(QApplication::translate("MainWindow", "\346\226\260\345\273\272\345\210\227", 0));
+        ___qtablewidgetitem5->setText(QApplication::translate("MainWindow", "Bid Size", 0));
         QTableWidgetItem *___qtablewidgetitem6 = tableWidget->horizontalHeaderItem(6);
-        ___qtablewidgetitem6->setText(QApplication::translate("MainWindow", "\344\271\260\344\270\200\344\273\267", 0));
+        ___qtablewidgetitem6->setText(QApplication::translate("MainWindow", "Bid", 0));
         QTableWidgetItem *___qtablewidgetitem7 = tableWidget->horizontalHeaderItem(7);
-        ___qtablewidgetitem7->setText(QApplication::translate("MainWindow", "\345\215\226\344\270\200\344\273\267", 0));
+        ___qtablewidgetitem7->setText(QApplication::translate("MainWindow", "Ask", 0));
         QTableWidgetItem *___qtablewidgetitem8 = tableWidget->horizontalHeaderItem(8);
-        ___qtablewidgetitem8->setText(QApplication::translate("MainWindow", "\345\215\226\344\270\200\351\207\217 ", 0));
+        ___qtablewidgetitem8->setText(QApplication::translate("MainWindow", "Ask Size", 0));
         QTableWidgetItem *___qtablewidgetitem9 = tableWidget->horizontalHeaderItem(9);
-        ___qtablewidgetitem9->setText(QApplication::translate("MainWindow", "\346\230\250\346\224\266\344\273\267", 0));
+        ___qtablewidgetitem9->setText(QApplication::translate("MainWindow", "Yest. Close", 0));
         QTableWidgetItem *___qtablewidgetitem10 = tableWidget->horizontalHeaderItem(10);
-        ___qtablewidgetitem10->setText(QApplication::translate("MainWindow", "\345\274\200\347\233\230\344\273\267", 0));
+        ___qtablewidgetitem10->setText(QApplication::translate("MainWindow", "Open", 0));
         QTableWidgetItem *___qtablewidgetitem11 = tableWidget->horizontalHeaderItem(11);
-        ___qtablewidgetitem11->setText(QApplication::translate("MainWindow", "\346\234\200\351\253\230\344\273\267", 0));
+        ___qtablewidgetitem11->setText(QApplication::translate("MainWindow", "High", 0));
         QTableWidgetItem *___qtablewidgetitem12 = tableWidget->horizontalHeaderItem(12);
-        ___qtablewidgetitem12->setText(QApplication::translate("MainWindow", "\346\234\200\344\275\216\344\273\267", 0));
+        ___qtablewidgetitem12->setText(QApplication::translate("MainWindow", "Low", 0));
         QTableWidgetItem *___qtablewidgetitem13 = tableWidget->horizontalHeaderItem(13);
-        ___qtablewidgetitem13->setText(QApplication::translate("MainWindow", "\346\227\266\351\227\264", 0));
+        ___qtablewidgetitem13->setText(QApplication::translate("MainWindow", "Time", 0));
         QTableWidgetItem *___qtablewidgetitem14 = tableWidget->horizontalHeaderItem(14);
-        ___qtablewidgetitem14->setText(QApplication::translate("MainWindow", "\346\235\245\346\272\220", 0));
-        label_2->setText(QApplication::translate("MainWindow", "\346\211\213\345\212\250\344\272\244\346\230\223", 0));
-        Label->setText(QApplication::translate("MainWindow", "\345\220\210\347\272\246\344\273\243\347\240\201", 0));
-        Label_2->setText(QApplication::translate("MainWindow", "\345\220\210\347\272\246\345\220\215\347\247\260", 0));
-        Label_3->setText(QApplication::translate("MainWindow", "\345\220\210\347\272\246\347\261\273\345\236\213", 0));
+        ___qtablewidgetitem14->setText(QApplication::translate("MainWindow", "Source", 0));
+        label_2->setText(QApplication::translate("MainWindow", "Discretionary", 0));
+        Label->setText(QApplication::translate("MainWindow", "Symbol", 0));
+        Label_2->setText(QApplication::translate("MainWindow", "Name", 0));
+        Label_3->setText(QApplication::translate("MainWindow", "Security Type", 0));
         ComboBoxContractType->clear();
         ComboBoxContractType->insertItems(0, QStringList()
-         << QApplication::translate("MainWindow", "\350\202\241\347\245\250", 0)
-         << QApplication::translate("MainWindow", "\346\234\237\350\264\247", 0)
-         << QApplication::translate("MainWindow", "\346\234\237\346\235\203", 0)
-         << QApplication::translate("MainWindow", "\345\244\226\346\261\207", 0)
+         << QApplication::translate("MainWindow", "Stock", 0)
+         << QApplication::translate("MainWindow", "Future", 0)
+         << QApplication::translate("MainWindow", "Option", 0)
+         << QApplication::translate("MainWindow", "Forex", 0)
         );
-        Label_4->setText(QApplication::translate("MainWindow", "\346\226\271\345\220\221", 0));
+        Label_4->setText(QApplication::translate("MainWindow", "Direction", 0));
         ComboBoxDirection->clear();
         ComboBoxDirection->insertItems(0, QStringList()
-         << QApplication::translate("MainWindow", "\345\244\232", 0)
-         << QApplication::translate("MainWindow", "\347\251\272", 0)
+         << QApplication::translate("MainWindow", "Long", 0)
+         << QApplication::translate("MainWindow", "Short", 0)
         );
-        Label_5->setText(QApplication::translate("MainWindow", "\345\274\200\345\271\263", 0));
+        Label_5->setText(QApplication::translate("MainWindow", "Order Flag", 0));
         ComboBox_BuyOrSell->clear();
         ComboBox_BuyOrSell->insertItems(0, QStringList()
-         << QApplication::translate("MainWindow", "\345\274\200\344\273\223", 0)
-         << QApplication::translate("MainWindow", "\345\271\263\344\273\223", 0)
-         << QApplication::translate("MainWindow", "\345\271\263\346\230\250", 0)
-         << QApplication::translate("MainWindow", "\345\271\263\344\273\212", 0)
+         << QApplication::translate("MainWindow", "Open", 0)
+         << QApplication::translate("MainWindow", "Close", 0)
+         << QApplication::translate("MainWindow", "Close Yesterday", 0)
+         << QApplication::translate("MainWindow", "Close Today", 0)
         );
-        Label_6->setText(QApplication::translate("MainWindow", "\344\273\267\346\240\274", 0));
-        Label_7->setText(QApplication::translate("MainWindow", "\346\225\260\351\207\217", 0));
-        Label_8->setText(QApplication::translate("MainWindow", "\346\214\207\344\273\244\347\261\273\345\236\213", 0));
+        Label_6->setText(QApplication::translate("MainWindow", "Price", 0));
+        Label_7->setText(QApplication::translate("MainWindow", "Quantity", 0));
+        Label_8->setText(QApplication::translate("MainWindow", "Order Type", 0));
         ComboBox_OrderType->clear();
         ComboBox_OrderType->insertItems(0, QStringList()
-         << QApplication::translate("MainWindow", "\345\270\202\344\273\267\346\214\207\344\273\244", 0)
-         << QApplication::translate("MainWindow", "\351\231\220\344\273\267\346\214\207\344\273\244", 0)
+         << QApplication::translate("MainWindow", "Market", 0)
+         << QApplication::translate("MainWindow", "Limit", 0)
         );
-        Label_9->setText(QApplication::translate("MainWindow", "\344\272\244\346\230\223\346\211\200", 0));
-        Label_10->setText(QApplication::translate("MainWindow", "\350\264\246\346\210\267", 0));
-        pushButton_Order->setText(QApplication::translate("MainWindow", "\344\270\213\345\215\225", 0));
-        tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("MainWindow", "\346\227\245\345\277\227", 0));
-        tabWidget->setTabText(tabWidget->indexOf(tab_2), QApplication::translate("MainWindow", "\345\247\224\346\211\230", 0));
-        tabWidget->setTabText(tabWidget->indexOf(tab_3), QApplication::translate("MainWindow", "\346\210\220\344\272\244", 0));
-        tabWidget->setTabText(tabWidget->indexOf(tab_4), QApplication::translate("MainWindow", "\346\214\201\344\273\223", 0));
-        tabWidget->setTabText(tabWidget->indexOf(tab_5), QApplication::translate("MainWindow", "\345\270\220\346\210\267", 0));
-        label->setText(QApplication::translate("MainWindow", "\350\207\252\345\212\250\344\272\244\346\230\223", 0));
+        Label_9->setText(QApplication::translate("MainWindow", "Exchange", 0));
+        Label_10->setText(QApplication::translate("MainWindow", "Account", 0));
+        pushButton_Order->setText(QApplication::translate("MainWindow", "Place Order", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("MainWindow", "Log", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab_2), QApplication::translate("MainWindow", "Order", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab_3), QApplication::translate("MainWindow", "Fill", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab_4), QApplication::translate("MainWindow", "Position", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab_5), QApplication::translate("MainWindow", "Account", 0));
+        label->setText(QApplication::translate("MainWindow", "Automatic", 0));
         QTableWidgetItem *___qtablewidgetitem15 = tableWidget_2->horizontalHeaderItem(0);
-        ___qtablewidgetitem15->setText(QApplication::translate("MainWindow", "\347\255\226\347\225\245ID", 0));
+        ___qtablewidgetitem15->setText(QApplication::translate("MainWindow", "SID", 0));
         QTableWidgetItem *___qtablewidgetitem16 = tableWidget_2->horizontalHeaderItem(1);
-        ___qtablewidgetitem16->setText(QApplication::translate("MainWindow", "\347\255\226\347\225\245\345\220\215\347\247\260", 0));
+        ___qtablewidgetitem16->setText(QApplication::translate("MainWindow", "Name", 0));
         QTableWidgetItem *___qtablewidgetitem17 = tableWidget_2->horizontalHeaderItem(2);
-        ___qtablewidgetitem17->setText(QApplication::translate("MainWindow", "\346\214\201\344\273\223\346\225\260\351\207\217", 0));
+        ___qtablewidgetitem17->setText(QApplication::translate("MainWindow", "nHoldings", 0));
         QTableWidgetItem *___qtablewidgetitem18 = tableWidget_2->horizontalHeaderItem(3);
-        ___qtablewidgetitem18->setText(QApplication::translate("MainWindow", "\344\272\244\346\230\223\346\225\260", 0));
+        ___qtablewidgetitem18->setText(QApplication::translate("MainWindow", "nTrades", 0));
         QTableWidgetItem *___qtablewidgetitem19 = tableWidget_2->horizontalHeaderItem(4);
-        ___qtablewidgetitem19->setText(QApplication::translate("MainWindow", "\347\212\266\346\200\201", 0));
-        pushButton->setText(QApplication::translate("MainWindow", "\345\274\200\345\220\257\347\255\226\347\225\245", 0));
-        pushButton_2->setText(QApplication::translate("MainWindow", "\346\232\202\345\201\234\347\255\226\347\225\245", 0));
-        pushButton_3->setText(QApplication::translate("MainWindow", "\345\201\234\346\255\242\347\255\226\347\225\245", 0));
-        pushButton_4->setText(QApplication::translate("MainWindow", "\346\270\205\344\273\223\347\255\226\347\225\245", 0));
-        menu->setTitle(QApplication::translate("MainWindow", "\346\226\207\344\273\266", 0));
+        ___qtablewidgetitem19->setText(QApplication::translate("MainWindow", "Open PnL", 0));
+        QTableWidgetItem *___qtablewidgetitem20 = tableWidget_2->horizontalHeaderItem(5);
+        ___qtablewidgetitem20->setText(QApplication::translate("MainWindow", "Close PnL", 0));
+        QTableWidgetItem *___qtablewidgetitem21 = tableWidget_2->horizontalHeaderItem(6);
+        ___qtablewidgetitem21->setText(QApplication::translate("MainWindow", "Status", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "Start Strat", 0));
+        pushButton_2->setText(QApplication::translate("MainWindow", "Pause Strat", 0));
+        pushButton_3->setText(QApplication::translate("MainWindow", "Stop Strat", 0));
+        pushButton_4->setText(QApplication::translate("MainWindow", "Liquidate Strat", 0));
+        menu->setTitle(QApplication::translate("MainWindow", "File", 0));
     } // retranslateUi
 
 };
